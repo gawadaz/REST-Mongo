6612c76c0dcf0441af8fce21325700f5830d3702
gawadaz <gawad.azzam@gmail.com>
2017-12-13 21:57:23 +0200

    first commit

---
+++ src/main/java/com/gawad/restmongo/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.gawad.restmongo;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+//@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@Repository
+public interface PersonRepository extends MongoRepository<Person, String> {
+
+	List<Person> findByLastName(@Param("name") String name);
+}
---
+++ src/main/java/com/gawad/restmongo/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.gawad.restmongo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gawad.restmongo.Person;
+import com.gawad.restmongo.PersonRepository;
+
+@RestController
+@RequestMapping("api")
+public class TestController {
+
+	@Autowired
+	PersonRepository personRepo;
+	
+	@RequestMapping("/person")
+	public List<Person> getAllPerson() {
+		return this.personRepo.findAll();
+	}
+	
+	@RequestMapping("/test")
+	public ResponseEntity<String> getTest() {
+		return new ResponseEntity<String>("test passed", HttpStatus.BAD_REQUEST);
+	}
+	
+}
